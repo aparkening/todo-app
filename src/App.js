3e3cc6e44e4a63c60798fedd8834675e283c3d46
aparkening <aaron.parkening@gmail.com>
2020-07-13 11:00:55 -0400

    Stub initial functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import TodoForm from "./TodoForm";
+// import TodoList from "./TodoList";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+  
+  // Add Todo
+  addTodo = (newTodo) => {
+    // Assign id
+    newTodo = {...newTodo, id: Date.now()}
+
+    // Add todo to state
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  render() {
+    // console.log("App State")
+    // console.log(this.state)    
+    return (
+      <div className="container">
+        {/* <TodoForm addTodo={this.addTodo}/>
+        <TodoList listName="today" updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
+        <TodoList listName="tomorrow" updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
+        <TodoList listName="thisWeek" updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
+        <TodoList listName="noDate" updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
+        <TodoList listName="completed" updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
