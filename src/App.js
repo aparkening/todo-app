4be4640bc6fb8031f791e21cac01859ac7c0b824
aparkening <aaron.parkening@gmail.com>
2020-07-13 12:49:38 -0400

    Filter todo lists and improve displayTitle method

--- src/App.js
+++ src/App.js
@@ -23,12 +23,54 @@ class App extends Component {
     });
   }
 
-  // Filter lists
+  // Return capitalized readable title based on listName
+  displayTitle = (name) => {
+    let title;
+    switch (name) {
+      case 'noDate':
+        title = 'No Date'
+        break;
+      case 'thisWeek':
+        title = 'This Week'
+        break;
+      default:
+        title = name.charAt(0).toUpperCase() + name.slice(1)
+      break;
+    }
+    return title;
+  }
 
-  // Generate list components
+  // Return filtered todos array
+  filterList = (name) => {
+    let list;
+    const now = new Date();
+    const today = now.setDate(now.getDay() + 1)
+    const tomorrow = now.setDate(now.getDay() + 2)
+    const thisWeek = now.setDate(now.getDay() + 7)
+    switch (name) {
+      case 'today':
+        list = this.state.todos.filter(todo => (todo.due <= today) && (todo.due !== ""));
+        break;
+      case 'tomorrow':
+        list = this.state.todos.filter(todo => (todo.due <= tomorrow) && (todo.due > today));
+        break;
+      case 'thisWeek':
+        list = this.state.todos.filter(todo => (todo.due <= thisWeek) && (todo.due > tomorrow));
+        break;
+      case 'completed':
+        list = this.state.todos.filter(todo => todo.due === "complete");
+        break;
+      default:
+        list = this.state.todos.filter(todo => todo.due === "");
+        break;
+    }
+    return list;
+  }
+
+  // Return TodoList components with listName, displayTitle, and filtered todos
   listComponents = () => {
-    const lists = ['Today', 'Tomorrow', 'ThisWeek', 'NoDate', 'Completed'];
-    return lists.map(name => <TodoList key={name} listName={name} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} todos={this.state.todos} />);
+    const lists = ['today', 'tomorrow', 'thisWeek', 'noDate', 'completed'];
+    return lists.map(name => <TodoList key={name} listName={name} displayTitle={this.displayTitle(name)} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} todos={this.filterList(name)} />);
   }
 
   render() {
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,29 +6,12 @@ const TodoList = props => {
   // Display todos as li elements
   const displayTodos = props.todos.map(obj => <li key={obj.id}>{obj.description}</li>)
   
-  // Display readable title based on listName
-  const makeTitle = () => {
-    let title;
-    switch (props.listName) {
-      case 'NoDate':
-        title = 'No Date'
-        break;
-      case 'ThisWeek':
-        title = 'This Week'
-        break;
-      default:
-        title = props.listName
-      break;
-    }
-    return title;
-  }
-
   console.log("TodoList props")
   console.log(props)
 
   return ( 
     <div className="list">
-      <h2>{makeTitle()}</h2>
+      <h2>{props.displayTitle}</h2>
       <div>{props.todos.length} todos</div>
       <ul>{displayTodos}</ul>
     </div>
