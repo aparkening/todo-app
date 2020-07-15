1094ecb65a8438646a2960a52d5506c33cdce429
aparkening <aaron.parkening@gmail.com>
2020-07-15 10:34:49 -0400

    Add ariaHidden and focusable to svgs

--- src/Todo.js
+++ src/Todo.js
@@ -19,7 +19,7 @@ const Todo = props => {
         <Col className="complete-todo" xs={1}>
           <button type="button" value="completed" className="complete" onClick={(e)=> handleTimeChange(e)} disabled={props.list === "completed" ? true : false} >
           {props.list === "completed" ? 
-            <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg" ariaHidden="true" focusable="false">
               <path d="M17 1L6 12L1 7" stroke="#03CEA4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg> : null}
             <span className="sr-only">Complete Todo</span>
@@ -40,7 +40,7 @@ const Todo = props => {
         </Col>
         <Col className="last">
           <button type="button" className="delete" onClick={() => props.deleteTodo(props.id)}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ariaHidden="true" focusable="false">
               <path d="M18 6L6 18" stroke="#9D9FA7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M6 6L18 18" stroke="#9D9FA7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
