08a23ec5283c541e458eb5c01ea86e4d22ec90ae
aparkening <aaron.parkening@gmail.com>
2020-09-04 11:55:02 -0400

    Add more default examples

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,25 +1,27 @@
 const initialData = {
   tasks: {
-    'task-1': { id: 'task-1', content: 'Refill chicken water' }, 
-    'task-2': { id: 'task-2', content: 'Refill rabbit water' }, 
-    'task-3': { id: 'task-3', content: 'Feed animals' }, 
-    'task-4': { id: 'task-4', content: 'Move rabbit fence' }, 
+    'task-1': { id: 'task-1', content: 'Make breakfast' }, 
+    'task-2': { id: 'task-2', content: 'Make lunch' }, 
+    'task-3': { id: 'task-3', content: 'Make 5 dinners' }, 
+    'task-4': { id: 'task-4', content: 'Eat breakfast!' }, 
+    'task-5': { id: 'task-5', content: 'Eat lunch!' }, 
+    'task-6': { id: 'task-6', content: 'Eat 5 dinners!' }, 
   },
   lists: {
     'list-today': {
       id: 'list-today',
       title: 'Today',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+      taskIds: ['task-1', 'task-4']
     },
     'list-tomorrow': {
       id: 'list-tomorrow',
       title: 'Tomorrow',
-      taskIds: []
+      taskIds: ['task-2', 'task-5']
     },
     'list-week': {
       id: 'list-week',
       title: 'This Week',
-      taskIds: []
+      taskIds: ['task-3', 'task-6']
     },
     'list-no': {
       id: 'list-no',
